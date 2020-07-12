d400af571304f965c7c142d11269139afe5a95c8
sourcedevil <wizard@sourcedevil.com>
2020-07-11 19:58:06 -0500

    modificaciones finales antes de entrega

--- src/main/java/com/sourcedevil/ref/rhoar/camel/gateway/Application.java
+++ src/main/java/com/sourcedevil/ref/rhoar/camel/gateway/Application.java
@@ -1,17 +1,11 @@
 package com.sourcedevil.ref.rhoar.camel.gateway;
 
-import org.apache.camel.opentracing.starter.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.*;
 
 @SpringBootApplication
-@CamelOpenTracing
 public class Application {
-
-    /**
-     * A main method to start this application.
-     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
